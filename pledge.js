08ac84e7689ddf9840422a739a89d6f800f98e14
jl975 <jlee6891@gmail.com>
2015-08-17 12:43:47 -0400

    first 2 chapters complete

--- pledge.js
+++ pledge.js
@@ -23,3 +23,57 @@ var pledge = require('pledge');
 var myDeferral = pledge.defer();
 var myPromise1 = myDeferral.$promise;
 --------------------------------------------------------*/
+
+function $Promise() {
+	this.state = 'pending';
+	this.value;
+	this.handlerGroups = [];
+}
+
+function Deferral() {
+	this.$promise = new $Promise();
+}
+
+function defer() {
+	return new Deferral();
+}
+
+Deferral.prototype.resolve = function(data) {
+	if (this.$promise.state != 'resolved' && this.$promise.state != 'rejected') {
+		this.$promise.value = data;
+		this.$promise.state = 'resolved';
+		if (this.$promise.handlerGroups.length) {
+			this.$promise.callHandlers();
+		}
+
+	}
+
+}; 
+
+Deferral.prototype.reject = function(reason) {
+	if (this.$promise.state != 'rejected' && this.$promise.state != 'resolved') {
+		this.$promise.value = reason;
+		this.$promise.state = 'rejected';
+	}
+};
+
+$Promise.prototype.then = function(success_cb, error_cb) {
+	if (typeof success_cb != 'function') success_cb = false;
+	if (typeof error_cb != 'function') error_cb = false;
+	this.handlerGroups.push({successCb: success_cb, errorCb: error_cb});
+
+	if (this.state == 'resolved' && success_cb) {
+		this.callHandlers()
+	}
+
+
+}
+
+$Promise.prototype.callHandlers = function() {
+	while (this.handlerGroups.length) {
+		var success_fn = this.handlerGroups.shift().successCb;
+		success_fn(this.value);
+	}
+};
+
+
